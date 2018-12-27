98b2c37073f865c2c6723e6f6e8b5d90a2e12906
Chase Farmer <chasefarmer2808@ufl.edu>
2018-12-27 14:14:14 -0500

    neopixels turn on

--- server/app.py
+++ server/app.py
@@ -1,10 +1,21 @@
+import board
+import neopixel
+import atexit
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+pixels = neopixel.NeoPixel(board.D18, 3, brightness=0.2, pixel_order=neopixel.GRB)
+
+@atexit.register
+def detach_pixels():
+    pixels.deinit()
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    pixels[0] = (255, 0, 0)
+    pixels.show()
     app.run(debug=True, host='0.0.0.0')
