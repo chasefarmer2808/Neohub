aa652deff834cf0aa3b7dad3616976cb5c15d7bc
chasefarmer2808 <chasefarmer2808@ufl.edu>
2019-01-06 18:25:18 -0500

    WIP add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatIconModule, MatDialogModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatIconModule, MatDialogModule, MatFormFieldModule, MatSelectModule, MatOptionModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,12 @@ import { AddNeopixelDialogComponent } from './dashboard/add-neopixel-dialog/add-
     AppRoutingModule,
     MatButtonModule,
     MatIconModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   entryComponents: [
     AddNeopixelDialogComponent
--- src/app/dashboard/add-neopixel-dialog/add-neopixel-dialog.component.ts
+++ src/app/dashboard/add-neopixel-dialog/add-neopixel-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-neopixel-dialog',
@@ -8,10 +9,20 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class AddNeopixelDialogComponent implements OnInit {
 
+  addNeopixelForm: FormGroup;
+  pixelTypeOptions: Array<string> = ['Strip', 'Ring', 'Matrix'];
+
   constructor(public dialogRef: MatDialogRef<AddNeopixelDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
+    this.createAddNeopixelForm();
+  }
+
+  createAddNeopixelForm() {
+    this.addNeopixelForm = new FormGroup({
+      pixelType: new FormControl(this.pixelTypeOptions[0], [Validators.required])
+    });
   }
 
 }
