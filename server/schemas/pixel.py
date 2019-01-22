6c29b197af3907afffca07cb36ce1610389193b2
Chase Farmer <chasefarmer2808@ufl.edu>
2019-01-21 22:07:05 -0500

    neopixel id

--- server/schemas/pixel.py
+++ server/schemas/pixel.py
@@ -1,10 +1,12 @@
 from marshmallow import Schema, fields
 
 class Pixel(object):
-    def __init__(self, index, color):
+    def __init__(self, neopixelId, index, color):
+        self.neopixelId = neopixelId
         self.index = index
         self.color = color
 
 class PixelSchema(Schema):
+    neopixelId = fields.Integer()
     index = fields.Integer()
     color = fields.List(fields.Integer())
