26cb13095b01a662365da8c35e3b220d2aa05b6e
chasefarmer2808 <chasefarmer2808@ufl.edu>
2019-01-28 21:23:38 -0500

    animation thread class

--- server/animations/animation_thread.py
+++ server/animations/animation_thread.py
@@ -1,1 +1,24 @@
 import time
+import threading
+
+
+class AnimationThread(threading.Thread):
+  def __init__(self, neopixel, animation):
+    threading.Thread.__init__(self)
+    self.neopixel = neopixel
+    self.animation = animation
+
+    # TODO: Validate animation.
+
+  def run(self):
+    i = 0
+
+    while True:
+      frame = self.animation[i]
+      colors = [tuple(c) for c in frame['colors']]
+
+      self.neopixel.neopixel[:] = colors
+      time.sleep(frame['duration'])
+
+      i += 1
+      i = i % len(self.animation)
--- server/resources/animation.py
+++ server/resources/animation.py
@@ -5,6 +5,8 @@ from flask_restful.reqparse import RequestParser
 
 from resources.strip import strips
 from schemas.animation import AnimationSchema
+from animations.animation_thread import AnimationThread
+
 
 class Animation(Resource):
     def __init__(self):
@@ -21,16 +23,19 @@ class Animation(Resource):
         with open(args['file']) as f:
             animation = json.load(f)
         
-        animSchema = AnimationSchema()
-        res = animSchema.load(animation)
+        anim_schema = AnimationSchema()
+        res = anim_schema.load(animation)
 
-        i = 0
-        while(True):
-            frame = res[0]['frames'][i]
-            colors = [tuple(c) for c in frame['colors']]
-            neopixel.neopixel[:] = colors
-            time.sleep(frame['duration'])
-            i += 1
-            i = i % 3
+        anim_thread = AnimationThread(neopixel, res[0]['frames'])
+        anim_thread.start()
+
+        # i = 0
+        # while(True):
+        #     frame = res[0]['frames'][i]
+        #     colors = [tuple(c) for c in frame['colors']]
+        #     neopixel.neopixel[:] = colors
+        #     time.sleep(frame['duration'])
+        #     i += 1
+        #     i = i % 3
 
         return res
