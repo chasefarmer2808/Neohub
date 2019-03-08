9ea83a572101237bff0a5b72dd68dff4016df972
Chase Farmer <chasefarmer2808@ufl.edu>
2019-03-08 10:23:56 -0500

    neopixel post tests

--- server/tests/functional/test_neopixel.py
+++ server/tests/functional/test_neopixel.py
@@ -15,4 +15,30 @@ def test_post_should_create_new_neopixel(test_client, init_database):
 
     assert response.status_code == 200
     assert len(response.data) > 0
-    # assert False
+
+    response = test_client.get('/api/strip')
+
+    assert response.status_code == 200
+    assert len(response.data) > 0
+
+def test_post_should_return_400_with_invalid_brightness(test_client):
+    data = {
+        'pin': 18,
+        'num_pixels': 6,
+        'brightness': 0.1
+    }
+
+    response = test_client.post('/api/strip', data=data)
+
+    assert response.status_code == 400
+
+def test_post_should_return_500_with_zero_brightness(test_client):
+    data = {
+        'pin': 18,
+        'num_pixels': 6,
+        'brightness': 0
+    }
+
+    response = test_client.post('/api/strip', data=data)
+
+    assert response.status_code == 500
